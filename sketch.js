4dbae625f97c9c3ce74b4bc8b028df90fda07990
Skylar Sweatt <sds19d@fsu.edu>
2022-10-31 12:04:21 -0400

    started

--- sketch.js
+++ sketch.js
@@ -1,7 +1,21 @@
+let y = 0;
+let speed = 3;
+
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
 }
 
 function draw() {
   background(125);
+
+  y += speed; 
+  rect(width/2,y,40,40);
+
+  If (y > 400 || y < 0) { 
+  speed *= -1;
+  }
 }
+
+
+
